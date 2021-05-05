788d5cf017caf9730b66418d033a9bb5dbf5434b
ofcat <klemenko.v@hotmail.com>
2021-05-05 15:02:51 +0200

    added Uptime example to test output

---
+++ src/main/java/at/example/Uptime.java
@@ -0,0 +1,28 @@
+package at.example;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/api/uptime")
+
+public class Uptime {
+
+    @GET
+    @Path("/{uptime}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String calculateUptime(@PathParam("uptime") double uptime) {
+        double minutesOfMonth = 60*24*30;
+        double definedUptimeInPercent = uptime / 100;
+        double uptimeInMinutes = minutesOfMonth * definedUptimeInPercent;
+        return String.format("%.2f", uptimeInMinutes);
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String calculateUptime() {
+        return this.calculateUptime(99.95);
+    }
+}
