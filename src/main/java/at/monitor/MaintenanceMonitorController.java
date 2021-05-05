288eb41a2e4d051a4d4523ed8c6aea038eb8c8bd
Thomas Karner <thomas.karner@fiskaly.com>
2021-05-05 18:53:20 +0200

    feature: add functionality to set a centrally stored message

--- src/main/java/at/monitor/MaintenanceMonitorController.java
+++ src/main/java/at/monitor/MaintenanceMonitorController.java
@@ -1,13 +1,31 @@
 package at.monitor;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
+import jakarta.ws.rs.*;
 
 @Path("/api")
 public class MaintenanceMonitorController {
+    private static String message = "red";
 
-    @GET
-    public String test() {
-        return "test";
+    @GET()
+    @Path("/reset")
+    public String resetMessage() {
+        message = "red";
+        System.out.println("reset: " + message);
+        return "message reset";
+    }
+
+    @GET()
+    @Path("/set/{newMessage}")
+    public String setMessage(@PathParam("newMessage") final String newMessage) {
+        message = newMessage;
+        System.out.println("set: " + message);
+        return message;
+    }
+
+    @GET()
+    @Path("/get")
+    public String getMessage() {
+        System.out.println("get: " + message);
+        return message;
     }
 }
