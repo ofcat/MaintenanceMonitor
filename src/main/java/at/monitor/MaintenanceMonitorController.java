6419790fdc328f9dbd26c4d764af3be6e42eefc0
ofcat <klemenko.v@hotmail.com>
2021-05-14 19:19:45 +0200

    feature: added frontend with html snippets, fixed callStatus() logic

--- src/main/java/at/monitor/MaintenanceMonitorController.java
+++ src/main/java/at/monitor/MaintenanceMonitorController.java
@@ -1,31 +1,93 @@
 package at.monitor;
 
 import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
 
-@Path("/api")
+@Path("")
 public class MaintenanceMonitorController {
-    private static String message = "red";
+    private static char message = ' ';
 
     @GET()
     @Path("/reset")
     public String resetMessage() {
-        message = "red";
-        System.out.println("reset: " + message);
-        return "message reset";
+        message = 'r';
+        return "Message reset";
     }
 
     @GET()
     @Path("/set/{newMessage}")
-    public String setMessage(@PathParam("newMessage") final String newMessage) {
+    public String setMessage(@PathParam("newMessage") final char newMessage) {
         message = newMessage;
-        System.out.println("set: " + message);
-        return message;
+        return callStatus(message);
     }
 
     @GET()
     @Path("/get")
     public String getMessage() {
-        System.out.println("get: " + message);
-        return message;
+
+        return callStatus(message);
     }
+
+    @GET
+    //@Path("/status")
+    @Produces({MediaType.TEXT_HTML})
+    public String callStatus(char message) {
+
+        if (message == '-')
+            return greenStatus();
+        else
+            return redStatus();
+    }
+
+
+    public String greenStatus() {
+
+        return """
+                <html>
+
+                <head>
+                <style>
+                h1 {text-align: center;}
+                p {text-align: center;}
+
+                </style>
+                    <meta charset="UTF-8">
+                    <title>Maintenance Monitor</title>
+                </head>
+
+                <body style="background-color:green;">
+                <br><br><br><br><br><br><br><br><br><br><br>
+                    <h1>Maintenance Monitor</h1>
+                    <p>status: ok</p>
+                </body>
+
+                </html>""";
+
+    }
+
+
+    public String redStatus() {
+
+        return """
+                <html>
+
+                <head>
+                <style>
+                h1 {text-align: center;}
+                p {text-align: center;}
+                </style>
+                    <meta charset="UTF-8">
+                    <title>Maintenance Monitor</title>
+                </head>
+
+                <body style="background-color:red;">
+                <br><br><br><br><br><br><br><br><br><br><br>
+                    <h1>Maintenance Monitor</h1>
+                    <p>status: error occurred</p>
+                </body>
+
+                </html>""";
+    }
+
+
 }
